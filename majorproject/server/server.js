baa8470661c4168271f7925a160b0e634b7787c2
pradip <pradipshrestha718@gmail.com>
2018-08-01 21:24:53 +0545

    changes comment out

--- majorproject/server/server.js
+++ majorproject/server/server.js
@@ -220,7 +220,7 @@ app.get("/queries/:id",function(req,res){
         if(err){
             console.log(err);
         } else {
-            console.log(query);
+            //console.log(query);
             //render show template with that campground
             //res.render("campgrounds/show", {campground: foundCampground});
             res.json(query);
