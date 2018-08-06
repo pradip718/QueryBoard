cf9e8b644ef8c2bebd3ad1572ca9a773dde47cca
pradip <pradipshrestha718@gmail.com>
2018-08-06 13:58:13 +0545

    added tags on client side

---
+++ majorproject/client/src/components/home/programmingLanguagesList.js
@@ -0,0 +1,18 @@
+export const PROGRAM = [
+    "ASP / ASP.NET",
+    "Java",
+    "C",
+    "C++",
+    "C#",
+    "FORTRAN",
+    "Javascript",
+    "Node.js",
+    "Bootstrap",
+    "jQuery",
+    "PostgreSQL",
+    "Python",
+    "R",
+    "Scala",
+    "SQL",
+    "SQLite"
+];
